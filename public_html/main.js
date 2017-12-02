49c42d4f335a71d557d9123f8bca2736bf413a60
Mehul Ajith <mehuajith7@gmail.com>
2017-12-02 08:06:34 -0500

    ui angul

--- public_html/main.js
+++ public_html/main.js
@@ -1,7 +1,10 @@
 function App($scope) {
  $scope.placeholder = 4;
+ $scope.logo_url = ""
  $scope.currentURL = "yooo"
  $scope.loader = false;
+ $scope.infoCard = false;
+ $scope.loadingTitle = "Searching for Company Data..."
 
  // Company data
  $scope.company = '';
@@ -10,6 +13,8 @@ function App($scope) {
  $scope.employees6moAgo = 0;
  $scope.totalfunding = 0;
  $scope.lastfundingdate = 0;
+ $scope.avgYearlyFunding = 0;
+ $scope.employeeChange = 0;
  $scope.founded = 0;
  $scope.industries = '';
 
@@ -35,20 +40,48 @@ function App($scope) {
    $scope.Market = true;
  };
 
+ $scope.getCompanyData = function() {
+   companyInfo();
+   $scope.loader = true;
+
+   setTimeout(function () {
+      $scope.infoCard = true;
+      $scope.loadingText = "Analyzing Data";
+      $scope.$apply();
+    }, 2000);
+
+    $scope.$apply();
+
+ };
+
+ // When key up
+ $( ".form-control" ).keyup(function() {
+   $scope.logo_url = 'http://logo.clearbit.com/' + $(this).val();
+   console.log($scope.logo_url);
+   $scope.$apply();
+ });
+
+
  // Getting Data for each tab category
 
- function comanyInfo() {
+ function companyInfo() {
 
-    $.getJSON('https://api.mattermark.com/domains/glide.com/companies?key=34cc41d72b44b5c8ed19ee44361829eeb2b53edcdb030731fef654f6048d26d8', function(data) {
+    $.getJSON('https://api.mattermark.com/domains/' + $('.form-control').val() + '/companies?key=34cc41d72b44b5c8ed19ee44361829eeb2b53edcdb030731fef654f6048d26d8', function(data) {
       console.log(data);
       $scope.company = data.name;
       $scope.description = data.description;
       $scope.employeesNow = data.employees;
       $scope.employees6moAgo = data.employees_6_months_ago;
+      $scope.employeeChange = (data.employees - data.employees_6_months_ago) / data.employees_6_months_ago;
       $scope.totalfunding = data.total_funding;
-      $scope.lastfundingdate = data.last_funding_date;
-      $scope.founded = data.est_founding_date;
+      var lastFunding = data.last_funding_date.substr(0, 4);
+      $scope.lastfundingdate = lastFunding;
+      var foundedYear = data.est_founding_date.substr(0, 4);
+      $scope.founded = foundedYear;
+      var avgFund = data.total_funding / foundedYear;
+      $scope.avgYearlyFunding = avgFund;
       $scope.industries = data.industries;
+      $scope.$apply();
     });
 
  }
