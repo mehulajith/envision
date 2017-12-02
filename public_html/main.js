8650e26dd0bb3812fce7d4172c975885b1ff7ee0
muramsudheer <mohammed.maazin@gmail.com>
2017-12-02 02:17:44 -0500

    Company Data

--- public_html/main.js
+++ public_html/main.js
@@ -4,32 +4,38 @@ function App($scope) {
  $scope.loader = false;
 
  // Company data
- $scope.company
- $scope.description
- $scope.employeesNow
- $scope.employees6moAgo
- $scope.totalfunding
- $scope.founded
- $scope.industries
+ $scope.company = '';
+ $scope.description = '';
+ $scope.employeesNow = 0;
+ $scope.employees6moAgo = 0;
+ $scope.totalfunding = 0;
+ $scope.founded = 0;
+ $scope.industries = '';
 
  //TabCategories
  $scope.Company = true;
  $scope.Funding = false;
  $scope.Market = false;
 
- $scope.getCompanyData = function() {
-   $scope.loader = true;
-   comanyInfo()
- };
-
 // Tabs
  $scope.companyTab = function() {
-   
+   $scope.Company = true;
+   $scope.Funding = false;
+   $scope.Market = false;
  };
  $scope.fundingTab = function() {
-
+   $scope.Company = false;
+   $scope.Funding = true;
+   $scope.Market = false;
  };
  $scope.marketTab = function() {
+   $scope.Company = false;
+   $scope.Funding = false;
+   $scope.Market = true;
+ };
+
+ // Getting Data for each tab category
+
 
  };
 
@@ -38,6 +44,13 @@ function App($scope) {
 
     $.getJSON('https://api.mattermark.com/domains/glide.com/companies?key=34cc41d72b44b5c8ed19ee44361829eeb2b53edcdb030731fef654f6048d26d8', function(data) {
       console.log(data);
+      $scope.company = data.name;
+      $scope.description = data.description;
+      $scope.employeesNow = data.employees;
+      $scope.employees6moAgo = data.employees_6_months_ago;
+      $scope.totalfunding = data.total_funding;
+      $scope.founded = data.est_founding_date;
+      $scope.industries = data.industries;
     });
 
  }
