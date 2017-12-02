caf41fbff9288144cd36feab36f41c3be90f4314
muramsudheer <mohammed.maazin@gmail.com>
2017-12-02 00:53:09 -0500

    jh,

--- public_html/main.js
+++ public_html/main.js
@@ -5,48 +5,39 @@ function App($scope) {
 
   $scope.getCompanyData = function() {
     $scope.loader = true;
+    comanyInfo()
   };
 
-  function doSearch() {
-  	ajax.open("GET", "https://autocomplete.clearbit.com/v1/companies/suggest?query=:" + input.value.trim(), true);
+  // function doSearch() {
+  // 	ajax.open("GET", "https://autocomplete.clearbit.com/v1/companies/suggest?query=:" + input.value.trim(), true);
+  // 		ajax.onload = function() {
+  // 			var sHTML = '';
+  //       console.log(ajax.responseText);
+  // 			JSON.parse(ajax.responseText).map(function(i) {
+  //
+  //         var logoArray = [i.logo]
+  //         //
+  //         // $scope.name = i.name
+  //         // $scope.domain = i.domain
+  //         // $scope.logo = i.logo
+  //
+  //         console.log(logoArray);
+  //
+  // 			});
+  // 			// result.innerHTML = sHTML;
+  // 		};
+  // 		ajax.send();
+  // }
+
+  function comanyInfo() {
+  	ajax.open("GET", "https://api.mattermark.com/domains/glide.com/companies?key=34cc41d72b44b5c8ed19ee44361829eeb2b53edcdb030731fef654f6048d26d8", true);
   		ajax.onload = function() {
   			var sHTML = '';
         console.log(ajax.responseText);
   			JSON.parse(ajax.responseText).map(function(i) {
-
-          var logoArray = [i.logo]
-          //
-          // $scope.name = i.name
-          // $scope.domain = i.domain
-          // $scope.logo = i.logo
-
-          console.log(logoArray);
-
+          console.log(i);
   			});
   			// result.innerHTML = sHTML;
   		};
   		ajax.send();
   }
-
-
-// Get URL of current tab w/ Chrome Extension
-// chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-//
-//      // since only one tab should be active and in the current window at once
-//      // the return variable should only have one entry
-//      var activeTab = arrayOfTabs[0];
-//      var activeTabId = activeTab.id; // or do whatever you need
-//      console.log(activeTab.id)
-//   });
-
-}
-
-// chrome.tabs.query({
-//     active: true,
-//     lastFocusedWindow: true
-// }, function(tabs) {
-//     // and use that tab to fill in out title and url
-//     var tab = tabs[0];
-//     console.log(tab.url);
-//     alert(tab.url);
-// });
