b2d08277eca9ae324d7b7a0615a78b573a529eeb
muramsudheer <mohammed.maazin@gmail.com>
2017-12-01 22:26:51 -0500

    update

--- public_html/main.js
+++ public_html/main.js
@@ -2,7 +2,7 @@
 function App($scope) {
   $scope.placeholder = 4;
 
-  
+
 
   function doSearch() {
 	ajax.open("GET", "https://autocomplete.clearbit.com/v1/companies/suggest?query=:" + input.value.trim(), true);
@@ -25,6 +25,26 @@ function App($scope) {
 		ajax.send();
 }
 
+
+// Get URL of current tab w/ Chrome Extension
+// chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+//
+//      // since only one tab should be active and in the current window at once
+//      // the return variable should only have one entry
+//      var activeTab = arrayOfTabs[0];
+//      var activeTabId = activeTab.id; // or do whatever you need
+//      console.log(activeTab.id)
+//   });
+
+chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+}, function(tabs) {
+    // and use that tab to fill in out title and url
+    var tab = tabs[0];
+    console.log(tab.url);
+    alert(tab.url);
+});
 
 
 }
